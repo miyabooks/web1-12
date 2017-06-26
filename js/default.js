283a2b7c88b8f431d48164165831ad7cd76c5950
miyabooks <pompomyamoto@yahoo.co.jp>
2017-06-26 17:43:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,3 +3,6 @@ for(var i = 1; i < 10; i++){
   li.textContent = i + '回目';
   document.getElementById('list').appendChild(li);
 }
+document.getElementById('form').onsubmit = function(){
+  window.alert(document.getElementById('form').word.value);
+};
